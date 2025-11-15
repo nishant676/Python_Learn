006ffdbd247d741746c590ae9642f3ddbb6d9f15
Nishant Mishra <88461593+nishant676@users.noreply.github.com>
2025-11-15 02:23:58 +0000

    Solved using if & else

---
+++ 03_Functions/qs_age.py
@@ -0,0 +1,11 @@
+age = input("Whts ur age?")
+age_score =  int(age)
+
+if age_score <13:
+    print("U r a chile")
+elif age_score< 20:
+    print("Teenager")
+elif  age_score <60:
+    print("adult") 
+else:
+    print("senior")   
